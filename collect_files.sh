3b6ed176913cfa6380162e94645c294d6bc7035f
daniil1602 <hidan027@yandex.ru>
2025-04-28 17:28:13 +0300

    all work

--- collect_files.sh
+++ collect_files.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+max_depth_p=-1
+in=$1
+out=$2
+while [ -n "$1" ]
+do
+    case "$1" in
+    "--max_depth" )
+            max_depth_p="$2"
+            ;;
+    esac
+    shift
+done
+if (( max_depth_p==-1)); then
+  python3 main.py $in $out 0
+else
+    python3 main.py $in $out $max_depth_p
+fi
+
+
